081f741ccdda4636dc61f743129d810cb36e703a
MisDarya <125642248+MisDarya@users.noreply.github.com>
2023-02-17 11:17:55 +0300

    New files

---
+++ main.py
@@ -0,0 +1,15 @@
+from roman import *
+
+t = ['IV', 'LVIII', 'MCMXCIV', 'XCIX', 'LXXX', 'LXIX']
+print('\nИсходные данные: ', t)
+
+print('\nКонвертируем из Римскмх в Арабские:')
+for i in t:
+  print(' ', i, '->' ,roman_to_int(i))
+
+a=[4, 58, 1994, 26, 99, 69, 80, 101, 3456, 78]
+print('\nКонвертируем из Арабских в Римские:')
+for i in a:
+  print(' ', i, '->',int_to_roman(i))
+
+print('\nЗадача выполнена!\n')
